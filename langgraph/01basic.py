1e60d64d4ef94a999c03243e9b8c10db007f84ba
Arup Saha <arup.saha007@gmail.com>
2026-08-31 22:18:09 +0530

    langgraph

---
+++ langgraph/01basic.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from dotenv import load_dotenv
+from IPython.display import Image, display
+import gradio as gr
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel
+import random
+
+print('Hello!!')
+
+# Some useful constants
+
+nouns = ["Cabbages", "Unicorns", "Toasters", "Penguins", "Bananas", "Zombies", "Rainbows", "Eels", "Pickles", "Muffins"]
+adjectives = ["outrageous", "smelly", "pedantic", "existential", "moody", "sparkly", "untrustworthy", "sarcastic", "squishy", "haunted"]
+
+# Our favorite first step! Crew was doing this for us, by the way.
+load_dotenv(override=True)
+
+
+def shout(text: Annotated[str, "something to be shouted"]) -> str:
+    print(text.upper())
+    return text.upper()
+
+shout("hello")
+
+class State(BaseModel):
+     messages: Annotated[list, add_messages]
